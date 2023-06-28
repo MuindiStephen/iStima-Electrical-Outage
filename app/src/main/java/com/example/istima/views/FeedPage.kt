7c30cc6f489b227e3c5181d134df5b90cd2371d6
marknjoroge <marknnjorogeh@gmail.com>
2023-06-28 14:29:42 +0300

    idk

--- app/src/main/java/com/example/istima/views/FeedPage.kt
+++ app/src/main/java/com/example/istima/views/FeedPage.kt
@@ -17,6 +17,9 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
