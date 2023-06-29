ede281adf304afed6a13a9a1c8e85a5b4c69f319
marknjoroge <marknnjorogeh@gmail.com>
2023-06-30 00:06:09 +0300

    working

--- app/src/main/java/com/example/istima/views/maps/MapFeed.kt
+++ app/src/main/java/com/example/istima/views/maps/MapFeed.kt
@@ -1,46 +1,62 @@
 package com.example.istima.views.maps
 
+import android.content.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.istima.views.maps.ui.theme.IStimaTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.MarkerOptions
+import com.google.maps.android.ktx.awaitMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MapFeed : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            IStimaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+@Composable
+fun MapFeed(navController: NavHostController) {
+    var message = ""
+    mapUI()
+}
+
+@Composable
+fun mapUI() {
+    val mapView = rememberMapViewWithLifecycle()
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        // on below line adding a map view to it.
+        AndroidView({ mapView }) { mapView ->
+            // on below line launching our map view
+            CoroutineScope(Dispatchers.Main).launch {
+                val map = mapView.awaitMap()
+                // on below line adding zoom controls for map.
+                map.uiSettings.isZoomControlsEnabled = true
+                // on below line we are creating a lat lng
+                // variable for sydney location
+                val sydney = LatLng(-35.016, 143.321)
+                // on below line adding a marker to map and
+                // specifying tile and position for it.
+                val markerOptions = MarkerOptions()
+                    .title("Sydney")
+                    .position(sydney)
+                map.addMarker(markerOptions)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    IStimaTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/istima/views/maps/MapPicker.kt
@@ -0,0 +1,4 @@
+package com.example.istima.views.maps
+
+class MapPicker {
+}
